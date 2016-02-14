5ded1220081e5b920daadba047c238ca233a31e3
Sichacvah <sichirc@gmail.com>
2016-02-14 11:57:41 +0400

    fix

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,13 +1,9 @@
 class Ticket < ActiveRecord::Base
-  #after_create :send_notification
+  after_create :send_notification
 
   private
 
   def send_notification
-
-    result = TicketMailer.recieve_mail(self).deliver_now
-    puts self.email
-    puts self.name
-    puts ENV['MAILGUN_LOGIN']
+    TicketMailer.recieve_mail(self).deliver
   end
 end
