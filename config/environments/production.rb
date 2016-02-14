f8eba84ee4c6a9494eebd16ad6a8a41b50b90128
Sichacvah <sichirc@gmail.com>
2016-02-14 12:40:05 +0400

    fix mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,7 +76,7 @@ Rails.application.configure do
 
 # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  config.action_mailer.default_url_options = { host: '127.0.0.1:3000' }
+  config.action_mailer.default_url_options = { host: 'helga-messages.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
 
   ActionMailer::Base.smtp_settings = {
